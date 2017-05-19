b2e60423d6ed89c4b73701144af178246cf560d4
Amarnath Rajasekaran <sharewithamar@gmail.com>
2017-05-19 14:57:14 +0530

    require own modules -basic

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,18 +1,21 @@
-console.log('starting app');
+console.log('starting app.js');
 const fs = require('fs');
 const os = require('os');
+const notes=require('./notes.js');
+let res= notes.add(10,5);
+console.log(res);
 
-let user = os.userInfo();
+/*let user = os.userInfo();
 
 console.log(user);
 
 
-fs.appendFile('message.txt', `Hello ${user.username} !`, (err) => {
+fs.appendFile('message.txt', `Hello ${user.username} ! your are ${notes.age}`, (err) => {
     if (err) { console.log('unable to write to file'); }
 
     console.log('The "data to append" was appended to file!');
-});
+});*/
 
 // or our use appendFileSync
 
-fs.appendFileSync('filesync.txt', 'Amar' + user.username+ '!');
+//fs.appendFileSync('filesync.txt', 'Amar' + user.username+ '!');
---
+++ notes-node/notes.js
@@ -0,0 +1,15 @@
+console.log('starting notes.js');
+
+//console.log(module);
+
+module.exports.age=29;
+
+module.exports.addNote = () => {
+
+console.log("add Note");
+return 'New Note';
+}
+
+module.exports.add = (a,b) => {
+    return a+b;
+}
