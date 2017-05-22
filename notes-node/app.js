dad2cfeb1d550e49176cbab302f47636ef10ea81
Amar <sharewithamar@gmail.com>
2017-05-22 20:07:00 +0530

    yargs arguments and notes functions base

--- notes-node/app.js
+++ notes-node/app.js
@@ -6,24 +6,22 @@ const yargs = require('yargs');
 const notes = require('./notes.js');
 
 const argv = yargs.argv;
-let command = process.argv[2];
-console.log('process',process.argv);
-console.log('yargs',argv);
-console.log('tile',argv.title);
-console.log('body',argv.body);
+let command = argv._[0];
+//console.log('process',process.argv);
+console.log('yargs', argv);
 
 
 if (command === 'add') {
-    console.log('add note');
+    notes.addNote(argv.title, argv.body);
 }
 else if (command === 'list') {
-    console.log('list all notes');
+    notes.getAll();
 }
 else if (command === 'read') {
-    console.log('read note');
+    notes.getNote(argv.title);
 }
 else if (command === 'remove') {
-    console.log('delete note');
+notes.removeNote(argv.title);
 }
 else {
     console.log('command not recognized');
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -2,9 +2,26 @@ console.log('starting notes.js');
 
 //console.log(module);
 
-module.exports.addNote = () => {
+var addNote =(title,body) => {
+console.log('Adding Note',title,body);
+}
 
-console.log("add Note");
-return 'New Note';
+var getAll=()=>{
+    console.log("get all");
+}
+
+var getNote =(title)=>{
+    console.log("get this title",title);
+}
+
+var removeNote = (title)=>{
+    console.log("remove this title",title);
+}
+
+module.exports={
+    addNote, // if both key and value have same name use ES6 syntax = addNote: addNote
+    getAll,
+    getNote,
+    removeNote
 }
 
