6de6acee68b5b3f776c30017d6ca26b037c09323
Amarnath Rajasekaran <sharewithamar@gmail.com>
2017-05-22 17:22:08 +0530

    install yargs

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,11 +2,16 @@ console.log('starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
-
+const yargs = require('yargs');
 const notes = require('./notes.js');
 
+const argv = yargs.argv;
 let command = process.argv[2];
-//console.log(process.argv);
+console.log('process',process.argv);
+console.log('yargs',argv);
+console.log('tile',argv.title);
+console.log('body',argv.body);
+
 
 if (command === 'add') {
     console.log('add note');
