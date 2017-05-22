7a34666cb260b642fbb93bab75b76f83fd4e2ed6
Amarnath Rajasekaran <sharewithamar@gmail.com>
2017-05-22 13:18:17 +0530

    reading command line arguments

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,6 +1,26 @@
 console.log('starting app.js');
 
 const fs = require('fs');
-const _=require('lodash');
+const _ = require('lodash');
 
-const notes=require('./notes.js');
+const notes = require('./notes.js');
+
+let command = process.argv[2];
+//console.log(process.argv);
+
+if (command === 'add') {
+    console.log('add note');
+}
+else if (command === 'list') {
+    console.log('list all notes');
+}
+else if (command === 'read') {
+    console.log('read note');
+}
+else if (command === 'remove') {
+    console.log('delete note');
+}
+else {
+    console.log('command not recognized');
+
+}
