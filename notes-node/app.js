da337dda1cad4a212a740411ecfd21d1585cb647
Amarnath Rajasekaran <sharewithamar@gmail.com>
2017-05-22 12:00:49 +0530

    nodemon installing and code clean

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,26 +1,6 @@
 console.log('starting app.js');
+
 const fs = require('fs');
-const os = require('os');
 const _=require('lodash');
 
-console.log(_.isString('Amar'));
-console.log(_.uniq(['Amar','Amar',1,1,2,3]));
-
-/*const notes=require('./notes.js');
-let res= notes.add(10,5);
-console.log(res);*/
-
-/*let user = os.userInfo();
-
-console.log(user);
-
-
-fs.appendFile('message.txt', `Hello ${user.username} ! your are ${notes.age}`, (err) => {
-    if (err) { console.log('unable to write to file'); }
-
-    console.log('The "data to append" was appended to file!');
-});*/
-
-// or our use appendFileSync
-
-//fs.appendFileSync('filesync.txt', 'Amar' + user.username+ '!');
+const notes=require('./notes.js');
