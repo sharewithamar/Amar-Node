b5d8c96032e4fdacbc8f4ded105c6f576da2eb9c
Amarnath Rajasekaran <sharewithamar@gmail.com>
2017-05-23 17:01:48 +0530

    arrow functions

---
+++ notes-node/playground/arrow-functions.js
@@ -0,0 +1,17 @@
+let square = x => x * x;
+console.log(square(9));
+
+var user = {
+    name: 'Amar',
+    sayHi: () => {
+        console.log(arguments);
+
+        console.log(`Hi i'm ${this.name}`);
+    },
+    sayHiAlt() {
+        console.log(arguments);
+        console.log(`Hi i'm ${this.name}`);
+    }
+};
+user.sayHi() // will return undefined cant access this.name
+//user.sayHiAlt(1, 2, 3);
