62a0545c0a9fab2f213f9c123141c653b88155ec
Amarnath Rajasekaran <sharewithamar@gmail.com>
2017-05-24 17:21:52 +0530

    error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -12,20 +12,30 @@ const argv = yargs
 
     })
     .help()
-    .alias('help','h')
+    .alias('help', 'h')
     .argv;
 
-    //console.log(argv);
+//console.log(argv);
 
-    let userAddress = encodeURIComponent(argv.a);
+let userAddress = encodeURIComponent(argv.a);
 
 request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${userAddress}`,
     json: true
 }, (error, response, body) => {
     // console.log(JSON.stringify(body,undefined,2));
-    console.log(`address: ${body.results[0].formatted_address}`);
-    console.log(`latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`longitude: ${body.results[0].geometry.location.lng}`);
+    if (error) {
+        console.log('Unable to connect to google servers');
+    }
+    else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find that address');
+
+    }
+    else if (body.status == 'OK') {
+        console.log(`address: ${body.results[0].formatted_address}`);
+        console.log(`latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`longitude: ${body.results[0].geometry.location.lng}`);
+    }
+
 
 });
