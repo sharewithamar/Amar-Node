9cfa43ae0d3ee8a385670f3c65df5fa04b97156a
Amarnath Rajasekaran <sharewithamar@gmail.com>
2017-05-24 14:26:28 +0530

    formatting objects JSON stringify

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,6 +4,6 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=%20Indiranagar%20Banglore',
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(JSON.stringify(body,undefined,2));
 
 });
