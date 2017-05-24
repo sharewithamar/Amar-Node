eb2bba7a157281f1e39fccaf5916c517978fbd01
Amarnath Rajasekaran <sharewithamar@gmail.com>
2017-05-24 11:15:16 +0530

    async basics

---
+++ weather-app/playground/async-basics.js
@@ -0,0 +1,12 @@
+console.log('starting app');
+
+setTimeout(() => {
+    console.log('inside of callback');
+}, 2000);
+
+setTimeout(() => {
+    console.log('second callback');
+
+}, 0);
+
+console.log('Finsihing up');
