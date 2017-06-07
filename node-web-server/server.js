4726b6370b56ecae4243b285bdbe0a08e86af159
Amar <sharewithamar@gmail.com>
2017-06-07 20:17:04 +0530

    Hello Express

---
+++ node-web-server/server.js
@@ -0,0 +1,28 @@
+const express= require('express');
+
+var app =express();
+
+app.get('/', function (req, res) {
+ // res.send('<h2>Hello Amar</h2>Hello World!')
+ res.send({
+     name: 'Amar',
+     likings:[
+        'Coding',
+        'Photography',
+        'Movies',
+        'History',
+        'Twitter'
+     ]
+ });
+});
+
+app.get('/about',(req,res)=>{
+    res.send('About Page1');
+});
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        errorMessage: `Avv that's bad url..`
+    });
+});
+app.listen(3000);
