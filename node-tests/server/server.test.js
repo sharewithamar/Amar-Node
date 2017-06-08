ead2149a027289a9c596f230753660d4cfc4d9a7
Amarnath Rajasekaran <sharewithamar@gmail.com>
2017-06-08 16:33:38 +0530

    describe

--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -2,41 +2,56 @@ const request = require('supertest');
 const app = require('./server').app;
 const expect = require('expect');
 
-it('should return hola response', (done) => {
 
 
-    /* request(app)
-         .get('/')
-         .expect(404)
-         .expect({
-             error: 'Page not found'
-         })
-         .end(done)*/
 
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                error: 'Page not found'
-            });
-        })
-        .end(done)
+describe('server', () => {
+
+    describe('GET /', () => {
+        it('should return hola response', (done) => {
+
+
+            /* request(app)
+                 .get('/')
+                 .expect(404)
+                 .expect({
+                     error: 'Page not found'
+                 })
+                 .end(done)*/
+
+            request(app)
+                .get('/')
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: 'Page not found'
+                    });
+                })
+                .end(done)
+
+        });
+
+
+    })
+
+    describe('GET /users', () => {
+        it('should return Max user object', (done) => {
+
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        name: 'Max',
+                        course: 'Angular'
+                    });
+                })
+                .end(done)
+
+        });
+    })
 
 });
 
 
-it('should return Max user object', (done) => {
 
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                name: 'Max',
-                course:'Angular'
-            });
-        })
-        .end(done)
-
-});
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,41 +1,60 @@
 const expect = require('expect');
 const utils = require('./utils');
 
-it('shoud add 2 numbers', () => {
-    let res = utils.add(10, 20);
-    // throw new Error('Sum is wrong');
-    //    if(res !==30)
-    //    throw new Error(`Expected 30,but got ${res}`);
-
-    expect(res).toBe(30).toBeA('number');
 
 
-});
+describe('Utils', () => {
 
-it('should add async add 2 numbers',(done)=>{
-    utils.asyncAdd(10,10,(sum)=>{
-        expect(sum).toBe(20).toBeA('number');
-        done();
+    describe('#add', () => {
+        it('shoud add 2 numbers', () => {
+            let res = utils.add(10, 20);
+            // throw new Error('Sum is wrong');
+            //    if(res !==30)
+            //    throw new Error(`Expected 30,but got ${res}`);
+
+            expect(res).toBe(30).toBeA('number');
+
+
+        });
     });
-});
 
 
-it('shoud square a number', () => {
-    let res = utils.square(10);
-    // throw new Error('Sum is wrong');
-    //    if(res !==100)
-    //    throw new Error(`Expected 100,but got ${res}`);
-    expect(res).toBe(100).toBeA('number');
 
-
-});
-
-it('should add async square a number',(done)=>{
-    utils.asyncSquare(10,(square)=>{
-        expect(square).toBe(100).toBeA('number');
-        done();
+    it('should add async add 2 numbers', (done) => {
+        utils.asyncAdd(10, 10, (sum) => {
+            expect(sum).toBe(20).toBeA('number');
+            done();
+        });
     });
+
+
+    it('shoud square a number', () => {
+        let res = utils.square(10);
+        // throw new Error('Sum is wrong');
+        //    if(res !==100)
+        //    throw new Error(`Expected 100,but got ${res}`);
+        expect(res).toBe(100).toBeA('number');
+
+
+    });
+
+    it('should add async square a number', (done) => {
+        utils.asyncSquare(10, (square) => {
+            expect(square).toBe(100).toBeA('number');
+            done();
+        });
+    });
+
+    it('should verify first and last names are set on obj', () => {
+        let user = { job: 'Senior Developer' };
+        let userObj = utils.setName(user, 'Amarnath Rajasekaran');
+        expect(userObj).toInclude({ firstName: 'Amarnath', lastName: 'Rajasekaran' }).toBeA('object');
+    });
+
+
+
 });
+
 
 it('should expect some values', () => {
     // expect(12).toNotBe(11);
@@ -44,21 +63,16 @@ it('should expect some values', () => {
     //  expect([2,3,4]).toInclude(2);
     //expect([2,3,4]).toExclude(1);
 
-/*    expect({
-        name:'Amar',
-        age:29,
-        location: 'Banglore'
-    }).toInclude({age:29});*/
+    /*    expect({
+            name:'Amar',
+            age:29,
+            location: 'Banglore'
+        }).toInclude({age:29});*/
     expect({
-        name:'Amar',
-        age:29,
+        name: 'Amar',
+        age: 29,
         location: 'Banglore'
-    }).toExclude({age:20});
+    }).toExclude({ age: 20 });
 });
 
 
-it('should verify first and last names are set on obj',()=>{
-    let user={ job: 'Senior Developer'};
-    let userObj = utils.setName(user,'Amarnath Rajasekaran');
-    expect(userObj).toInclude({firstName:'Amarnath',lastName:'Rajasekaran'}).toBeA('object');
-});
