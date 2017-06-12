ff4ee80f061eb34ea7da86624b0966b4bd2cdbd4
Amarnath Rajasekaran <sharewithamar@gmail.com>
2017-06-12 17:07:36 +0530

    GET /todos/:id

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 let { mongoose } = require('./db/mongoose');
 let { Todo } = require('./models/todo');
@@ -24,13 +25,34 @@ app.post('/todos', (req, res) => {
 
 });
 
-app.get('/todos',(req,res)=>{
-    Todo.find().then(todos=>{
-        res.send({todos});
-    },err=>{
-    res.status(400).send(err);
+app.get('/todos', (req, res) => {
+    Todo.find().then(todos => {
+        res.send({ todos });
+    }, err => {
+        res.status(400).send(err);
     })
 });
+
+
+app.get('/todos/:id', (req, res) => {
+
+    let paramId = req.params.id;
+
+
+    if (!ObjectID.isValid(paramId)) {
+        return res.status(404).send('');
+    }
+
+    Todo.findById(paramId).then(todo => {
+        if (!todo)
+            return res.status(404).send('');
+        //   console.log(JSON.stringify(todo, undefined, 2));
+        res.send(todo);
+
+    }).catch(e => res.status(404).send(''));
+
+});
+
 
 app.listen('3000', () => {
     console.log('Started on port 3000');
