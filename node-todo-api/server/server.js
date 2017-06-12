9f0161a42a8108da2db77fda8d1b19ad43378284
Amar <sharewithamar@gmail.com>
2017-06-12 07:17:37 +0530

    mongoose validation

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,33 +1,55 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-mongoose.promise= global.promise;
+mongoose.promise = global.promise;
 
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 let Todo = mongoose.model('Todos',
     {
-        text:{
-            type :String
+        text: {
+            type: String,
+            required: true,
+            minlength: 1,
+            trim: true
         },
-        completed:{
-            type:Boolean
+        completed: {
+            type: Boolean,
+            default: false
         },
-        completedAt:{
-            type: Number
+        completedAt: {
+            type: Number,
+            default: null
+
         }
     }
 );
 
+/*let newTodo = new Todo({
+    text: ' Good Morning  '
+});
+*/
 /*let newTodo= new Todo({
-    text: 'Cook Dinner'
-});*/
-
-let newTodo= new Todo({
     text: 'Eat Lunch',
     completed: false,
     completedAt: 123
     
+});*/
+
+
+//newTodo.save().then(doc => console.log(doc), err => console.log(err));
+
+let user = mongoose.model('users',{
+  email:{
+      type:String,
+      minlength:1,
+      trim:true,
+      required:true
+
+  }
 });
 
+let newUser = new user({
+    email: 'Amar '
+});
 
-newTodo.save().then(doc => console.log(doc),err=> console.log(err));
+newUser.save().then(doc => console.log(doc), err => console.log(err));
