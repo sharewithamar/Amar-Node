4574bba479aed2d48838991a616d6b4eb9b6d4a0
AmarBosch <amarnath.rajasekaran@in.bosch.com>
2017-06-12 13:02:31 +0530

    test POST /todos

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,30 +1,32 @@
 let express = require('express');
-let bodyParser= require('body-parser');
+let bodyParser = require('body-parser');
 
-let {mongoose} = require('./db/mongoose');
-let {Todo} = require('./models/todo');
-let {user}= require('./models/user');
+let { mongoose } = require('./db/mongoose');
+let { Todo } = require('./models/todo');
+let { user } = require('./models/user');
 
 
 let app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos',(req,res)=>{
-  //  console.log(req.body);
-  let todo = new Todo({
-      text: req.body.text
-  });
+app.post('/todos', (req, res) => {
+    //  console.log(req.body);
+    let todo = new Todo({
+        text: req.body.text
+    });
 
-  todo.save().then(doc =>{
-      res.send(doc);
-  },err=>{
-      res.status(400).send(err);
-  })
+    todo.save().then(doc => {
+        res.send(doc);
+    }, err => {
+        res.status(400).send(err);
+    })
 
 });
 
-app.listen('3000',()=>{
+app.listen('3000', () => {
     console.log('Started on port 3000');
 })
 
+
+module.exports = { app };
